d72be30b0b19dd73d233ed583013bd65dfc6dcbb
rockerbacon <vitor.santos@lab309.com.br>
2023-08-07 19:15:04 -0300

    Divide and conquer factorial implementation for showcasing the impact of algorithmic optimization

--- elaborate/3-factorial/c/biguint/factorial.c
+++ elaborate/3-factorial/c/biguint/factorial.c
@@ -6,13 +6,32 @@
 	b = tmp;\
 }
 
+void ranged_factorial(biguint_t* result_buffer, size_t range_start, size_t range_end) {
+	if (range_start == range_end) {
+		biguint_set(result_buffer, range_start);
+		return;
+	} else if (range_start > range_end) {
+		biguint_set(result_buffer, 1);
+		return;
+	}
+
+	biguint_t left_buffer;
+	biguint_t right_buffer;
+
+	biguint_init(&left_buffer);
+	biguint_init(&right_buffer);
+
+	size_t middle = (range_start + range_end) / 2;
+	ranged_factorial(&left_buffer, range_start, middle);
+	ranged_factorial(&right_buffer, middle + 1, range_end);
+
+	biguint_mult(result_buffer, &left_buffer, &right_buffer);
+
+	biguint_destroy(&left_buffer);
+	biguint_destroy(&right_buffer);
+}
+
 void biguint_factorial(biguint_t* result_buffer, uint32_t number) {
-	biguint_t additional_buffer;
-	biguint_t scalar_buffer;
-
-	biguint_t* factor_buffer;
-	biguint_t* product_buffer;
-
 	if (number < 3) {
 		biguint_set(
 			result_buffer,
@@ -21,26 +40,6 @@ void biguint_factorial(biguint_t* result_buffer, uint32_t number) {
 		return;
 	}
 
-	factor_buffer = &additional_buffer;
-	product_buffer = result_buffer;
-
-	biguint_init(&scalar_buffer);
-	biguint_init(&additional_buffer);
-
-	biguint_set(factor_buffer, 2);
-	for (uint32_t scalar = 3; scalar <= number; scalar++) {
-		biguint_set(&scalar_buffer, scalar);
-		biguint_mult(product_buffer, factor_buffer, &scalar_buffer);
-		swap_ptr(product_buffer, factor_buffer);
-	}
-
-	if (factor_buffer != result_buffer) {
-		swap_ptr(additional_buffer.value, result_buffer->value);
-		result_buffer->len = additional_buffer.len;
-		result_buffer->capacity = additional_buffer.capacity;
-	}
-
-	biguint_destroy(&scalar_buffer);
-	biguint_destroy(&additional_buffer);
+	ranged_factorial(result_buffer, 2, number);
 }
 
